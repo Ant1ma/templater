5a78f83b7c526a31af608fff443b9854d72eddc8
matian <redstonegnom1@gmail.com>
2023-05-20 09:50:54 +0300

    make struct

---
+++ src/template_user/dpnds/dependes.py
@@ -0,0 +1,7 @@
+import sys
+
+sys.path.append('../')
+from content_table import create_lists
+from create_content import create_content
+from extract_pic import extract_pictures
+from margins import create_margins
---
+++ src/template_user/dpnds/libraries.py
@@ -0,0 +1,10 @@
+import docx 
+from docx import *
+from docx.oxml import OxmlElement 
+from docx.shared import Inches, Cm, Mm, Pt
+from docx.oxml.shared import qn
+#from docxbookmark import DocumentCreate as dc 
+from docx.enum.text import WD_ALIGN_PARAGRAPH 
+import zipfile, pathlib 
+import os 
+
